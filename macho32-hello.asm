81940b3c2e94db2db2b009349eb2bc349c42e8d3
atmarksharp <atmarksharp@live.jp>
2016-09-03 21:44:46 +0900

    Added comments

--- macho32-hello.asm
+++ macho32-hello.asm
@@ -6,16 +6,17 @@ section .text
 start:
     push    dword msg.len
     push    dword msg
-    push    dword 1
-    mov     eax, 4
-    sub     esp, 4
-    int     0x80
-    add     esp, 16
+    push    dword 1 ; file descriptor value
+    mov     eax, 4 ; system call number for write
+    sub     esp, 4 ; OS X (and BSD) system calls needs "extra space" on stack
+    int     0x80 ; make the actual system call
+    ; clean up the stack
+    add     esp, 16 ; 3 args * 4 bytes/arg + 4 bytes extra space = 16 bytes
 
-    push    dword 0
-    mov     eax, 1
-    sub     esp, 12
-    int     0x80
+    push    dword 0 ; exit status returned to the operating system
+    mov     eax, 1 ; system call number for exit
+    sub     esp, 4 ; OS X (and BSD) system calls needs "extra space" on stack
+    int     0x80 ; make the system call
 
 section .data
 
